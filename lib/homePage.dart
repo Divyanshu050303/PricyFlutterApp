037d621edf19af8bfd6231c910f834fe6cd5f3e4
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2023-05-18 22:19:54 +0530

    Home Page

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -13,19 +13,21 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     MediaQueryData mediaQueryData = MediaQuery.of(context);
+    double iconSize=mediaQueryData.size.width*0.1;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          "Expense Tracker", style: TextStyle(color: Colors.black),),
+          "Expense Tracker",
+          style: TextStyle(color: Colors.white,fontSize: 25),
+        ),
         elevation: 0,
-        backgroundColor: Colors.transparent,
+        backgroundColor: Colors.cyan.shade200,
       ),
       body: Stack(
         children: [
           Container(
               padding: const EdgeInsets.only(top: 20),
-
-
               decoration: const BoxDecoration(
                 gradient: LinearGradient(
                   colors: [Colors.cyan, Colors.purpleAccent],
@@ -35,86 +37,179 @@ class _HomeState extends State<Home> {
               ),
               width: mediaQueryData.size.width,
               height: mediaQueryData.size.height,
-              child:
-              const Text("Overview",
-                style: TextStyle(fontSize: 35, color: Colors.black,),
-                textAlign: TextAlign.center,)
-          ),
-
-          Container(
-            padding: EdgeInsets.only(top: mediaQueryData.size.height * 0.001),
-            // alignment: Alignment.topCenter,
-            child: const PieChart(
-              dataMap: {
-                "Flutter": 5,
-                "React": 3,
-                "Xamarin": 3,
-                "Ionic": 2,
-              },
-              animationDuration: Duration(milliseconds: 800),
-              chartType: ChartType.ring,
-              ringStrokeWidth: 32,
-              centerText: "Expense",
-              centerTextStyle: TextStyle(color: Colors.black,
-                  fontSize: 20,
-                  backgroundColor: Colors.transparent),
-              chartRadius: 250,
-              chartValuesOptions: ChartValuesOptions(
-                showChartValueBackground: false,
-                showChartValues: true,
-                showChartValuesInPercentage: true,
-                showChartValuesOutside: true,
-                decimalPlaces: 1,
-              ),
-              legendOptions: LegendOptions(
-                showLegends: false,
-                showLegendsInRow: false,
-                legendPosition: LegendPosition.right,
-                legendTextStyle: TextStyle(
-                  fontWeight: FontWeight.bold,
+              child: const Text(
+                "Overview",
+                style: TextStyle(
+                  fontSize: 35,
+                  color: Colors.black,
                 ),
-              ),
-            ),
+                textAlign: TextAlign.center,
+              )),
+          const SizedBox(
+            height: 1,
           ),
+          Transform.translate(
+              offset: Offset(0, -70),
+              child: Container(
+                padding:
+                    EdgeInsets.only(top: mediaQueryData.size.height * 0.001),
+                // alignment: Alignment.topCenter,
+                // transform: Matrix4.rotationY(-0.3),
+
+                transformAlignment: Alignment.topCenter,
+                margin: const EdgeInsets.only(top: 2),
+
+                child:  PieChart(
+                  dataMap: const {
+                    "Flutter": 5,
+                    "React": 3,
+                    "Xamarin": 3,
+                    "Ionic": 2,
+                  },
+                  colorList: [Colors.purpleAccent.shade100,Colors.greenAccent.shade200,Colors.pinkAccent.shade200,Colors.blue.shade500],
+                  animationDuration: Duration(milliseconds: 800),
+                  chartType: ChartType.ring,
+                  ringStrokeWidth: 37,
+                  centerText: "Expense",
+                  centerTextStyle: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      backgroundColor: Colors.transparent),
+                  chartRadius: 250,
+                  chartValuesOptions: const ChartValuesOptions(
+                    showChartValueBackground: false,
+                    showChartValues: true,
+                    showChartValuesInPercentage: true,
+                    showChartValuesOutside: true,
+                    decimalPlaces: 1,
+                  ),
+                  legendOptions: const LegendOptions(
+                    showLegends: false,
+                    showLegendsInRow: false,
+                    legendPosition: LegendPosition.right,
+                    legendTextStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              )),
           Container(
-            padding: EdgeInsets.only(top: mediaQueryData.size.height * 0.6,
+            padding: EdgeInsets.only(
+                top: mediaQueryData.size.height * 0.46,
                 left: mediaQueryData.size.width * .01),
             // alignment: Alignment.bottomCenter,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
               children: [
+                SizedBox(
+                  height: 70,
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    ElevatedButton(onPressed: () {},
+                    SizedBox(
+                      width: mediaQueryData.size.width*0.4,
+                    height: mediaQueryData.size.height*.06,
+                    child:ElevatedButton(
+                      onPressed: () {},
                       style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.resolveWith<
-                            Color>(
-                              (Set<MaterialState> states) {
+                        backgroundColor:
+                            MaterialStateProperty.resolveWith<Color>(
+                          (Set<MaterialState> states) {
                             if (states.contains(MaterialState.pressed)) {
                               return Colors.blue;
-                            } else if (states.contains(
-                                MaterialState.disabled)) {
+                            } else if (states
+                                .contains(MaterialState.disabled)) {
                               return Colors.grey;
                             }
-                            return Colors.greenAccent;
+                            return Colors.purpleAccent.shade100;
                           },
                         ),
+
                       ),
-                      child: const Text(
-                          "Petroleum", style: TextStyle(fontSize: 20)),),
-                    ElevatedButton(onPressed: () {},
-                        child: const Text("Dairy", style: TextStyle(fontSize: 20)))
+
+                      child: const Text("Grocery",
+                          style: TextStyle(fontSize: 20)),
+                    )),    SizedBox(
+                      width: mediaQueryData.size.width*0.4,
+                    height: mediaQueryData.size.height*.06,
+                    child:ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.resolveWith<Color>(
+                          (Set<MaterialState> states) {
+                            if (states.contains(MaterialState.pressed)) {
+                              return Colors.blue;
+                            } else if (states
+                                .contains(MaterialState.disabled)) {
+                              return Colors.grey;
+                            }
+                            return Colors.greenAccent.shade200;
+                          },
+                        ),
+
+                      ),
+
+                      child: const Text("Petroleum",
+                          style: TextStyle(fontSize: 20)),
+                    )),
+
                   ],
-                ), Row(
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    ElevatedButton(onPressed: () {},
-                      child: const Text(
-                        "Petroleum", style: TextStyle(fontSize: 20),),),
-                    ElevatedButton(onPressed: () {},
-                        child: const Text("Dairy", style: TextStyle(fontSize: 20)))
+                    SizedBox(
+                        width: mediaQueryData.size.width*0.4,
+                        height: mediaQueryData.size.height*.06,
+                        child:ElevatedButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                            backgroundColor:
+                            MaterialStateProperty.resolveWith<Color>(
+                                  (Set<MaterialState> states) {
+                                if (states.contains(MaterialState.pressed)) {
+                                  return Colors.blue;
+                                } else if (states
+                                    .contains(MaterialState.disabled)) {
+                                  return Colors.grey;
+                                }
+                                return Colors.pinkAccent.shade200;
+                              },
+                            ),
+
+                          ),
+
+                          child: const Text("Dairy",
+                              style: TextStyle(fontSize: 20)),
+                        )),
+                    SizedBox(
+                        width: mediaQueryData.size.width*0.4,
+                        height: mediaQueryData.size.height*.06,
+                        child:ElevatedButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                            backgroundColor:
+                            MaterialStateProperty.resolveWith<Color>(
+                                  (Set<MaterialState> states) {
+                                if (states.contains(MaterialState.pressed)) {
+                                  return Colors.blue;
+                                } else if (states
+                                    .contains(MaterialState.disabled)) {
+                                  return Colors.grey;
+                                }
+                                return Colors.blue.shade500;
+                              },
+                            ),
+
+                          ),
+
+                          child: const Text("Other",
+                              style: TextStyle(fontSize: 20)),
+                        )),
                   ],
                 )
               ],
@@ -122,15 +217,24 @@ class _HomeState extends State<Home> {
           )
         ],
       ),
-      bottomNavigationBar: FluidNavBar(icons: [
-        FluidNavBarIcon(icon: Icons.home),
-        FluidNavBarIcon(icon: Icons.access_alarm_outlined),
-        FluidNavBarIcon(icon: Icons.tiktok),
-        FluidNavBarIcon(icon: Icons.settings),
-      ],
-        style: FluidNavBarStyle(barBackgroundColor: Colors.cyan.shade200,
-          iconUnselectedForegroundColor: Colors.white,),
+      bottomNavigationBar: FluidNavBar(
+        icons: [
+          FluidNavBarIcon(icon: Icons.home),
+          FluidNavBarIcon(icon: Icons.access_alarm_outlined),
+          FluidNavBarIcon(icon: Icons.tiktok),
+          FluidNavBarIcon(icon: Icons.settings),
+        ],
+        style: FluidNavBarStyle(
+          barBackgroundColor: Colors.cyan.shade200,
+          iconUnselectedForegroundColor: Colors.white,
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
