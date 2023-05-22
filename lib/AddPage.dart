db0b7481c5b52e4fac30d9e31a8a07a94210701e
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2023-05-22 21:27:58 +0530

    add icon to navigate to home screen

--- lib/AddPage.dart
+++ lib/AddPage.dart
@@ -1,7 +1,6 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pricyapp/NavBar.dart';
 
 import 'homePage.dart';
 
@@ -15,26 +14,27 @@ class AddItem extends StatefulWidget {
 const List<String> list = <String>['Grosery', "Petroleum", "Dairy", "Other"];
 
 class _AddItemState extends State<AddItem> {
-
-
   String dropdownValue = list.first;
+
   @override
   Widget build(BuildContext context) {
     MediaQueryData mediaQueryData = MediaQuery.of(context);
 
     return Scaffold(
-
       body: SingleChildScrollView(
         child: Stack(
           children: [
-
             Container(
               width: mediaQueryData.size.width,
               height: mediaQueryData.size.height,
               padding: EdgeInsets.only(
                   top: MediaQuery.of(context).size.height * 0.25,
-                  left: MediaQuery.of(context).size.width >400 ?mediaQueryData.size.width*0.35:mediaQueryData.size.width*.1,
-                  right: MediaQuery.of(context).size.width >400 ?mediaQueryData.size.width*0.35:mediaQueryData.size.width*.1),
+                  left: MediaQuery.of(context).size.width > 400
+                      ? mediaQueryData.size.width * 0.35
+                      : mediaQueryData.size.width * .1,
+                  right: MediaQuery.of(context).size.width > 400
+                      ? mediaQueryData.size.width * 0.35
+                      : mediaQueryData.size.width * .1),
               decoration: const BoxDecoration(
                 gradient: LinearGradient(
                   colors: [Colors.cyan, Colors.purpleAccent],
@@ -46,41 +46,47 @@ class _AddItemState extends State<AddItem> {
                 keyboardType: TextInputType.text,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10)
-                  ),
+                      borderRadius: BorderRadius.circular(10)),
                   focusedBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.black,
-                      width: 2
-                    )
-                  ),
+                      borderSide: BorderSide(color: Colors.black, width: 2)),
                   hintText: "Name of the item",
-                  label: Text("Item Name", style: TextStyle(color: Colors.black),),
+                  label: Text(
+                    "Item Name",
+                    style: TextStyle(color: Colors.black),
+                  ),
                   focusColor: Colors.black,
-
                 ),
               ),
             ),
             Container(
-
               padding: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.35,
-                  left: MediaQuery.of(context).size.width >400 ?mediaQueryData.size.width*0.35:mediaQueryData.size.width*.1,
-                 ),
-              width: MediaQuery.of(context).size.width >400 ?mediaQueryData.size.width*0.65:mediaQueryData.size.width*.9,
+                top: MediaQuery.of(context).size.height * 0.35,
+                left: MediaQuery.of(context).size.width > 400
+                    ? mediaQueryData.size.width * 0.35
+                    : mediaQueryData.size.width * .1,
+              ),
+              width: MediaQuery.of(context).size.width > 400
+                  ? mediaQueryData.size.width * 0.65
+                  : mediaQueryData.size.width * .9,
               child: DropdownButton<String>(
                   value: dropdownValue,
-                  icon: Padding(padding: EdgeInsets.only(left: mediaQueryData.size.width>1000?mediaQueryData.size.width*0.25:mediaQueryData.size.width*0.5),child: const Icon(Icons.arrow_drop_down,color: Colors.black,)),
+                  icon: Padding(
+                      padding: EdgeInsets.only(
+                          left: mediaQueryData.size.width > 1000
+                              ? mediaQueryData.size.width * 0.25
+                              : mediaQueryData.size.width * 0.5),
+                      child: const Icon(
+                        Icons.arrow_drop_down,
+                        color: Colors.black,
+                      )),
                   elevation: 30,
                   style: const TextStyle(
                     color: Colors.black,
-
                   ),
                   underline: Container(
                     height: 2,
                     width: 40,
                     color: Colors.black,
-
                   ),
                   onChanged: (String? value) {
                     setState(() {
@@ -89,46 +95,66 @@ class _AddItemState extends State<AddItem> {
                   },
                   items: list.map<DropdownMenuItem<String>>((String value) {
                     return DropdownMenuItem<String>(
-                        value: value, child:Padding(padding: EdgeInsets.only(left: mediaQueryData.size.width*.008),child:Text(value, style: TextStyle(fontSize: 15),) ,) );
+                        value: value,
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              left: mediaQueryData.size.width * .008),
+                          child: Text(
+                            value,
+                            style: TextStyle(fontSize: 15),
+                          ),
+                        ));
                   }).toList()),
             ),
             Container(
               padding: EdgeInsets.only(
                   top: MediaQuery.of(context).size.height * 0.45,
-                  left: MediaQuery.of(context).size.width >400 ?mediaQueryData.size.width*0.35:mediaQueryData.size.width*.1,
-                  right: MediaQuery.of(context).size.width >400 ?mediaQueryData.size.width*0.35:mediaQueryData.size.width*.1),
-
-              child:  TextField(
+                  left: MediaQuery.of(context).size.width > 400
+                      ? mediaQueryData.size.width * 0.35
+                      : mediaQueryData.size.width * .1,
+                  right: MediaQuery.of(context).size.width > 400
+                      ? mediaQueryData.size.width * 0.35
+                      : mediaQueryData.size.width * .1),
+              child: TextField(
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10)
+                      borderRadius: BorderRadius.circular(10)),
+                  focusedBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 2),
                   ),
-                    focusedBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Colors.black,
-                            width: 2
-                        ),
-
-                    ),
                   hintText: "Date",
-                  label: Text("Date",style: TextStyle(color: Colors.black)),
+                  label: Text("Date", style: TextStyle(color: Colors.black)),
                   focusColor: Colors.black,
-
                 ),
               ),
-
             ),
             SizedBox(
-              width: mediaQueryData.size.width>400?mediaQueryData.size.width*0.6:mediaQueryData.size.width*0.7,
-            height: mediaQueryData.size.height*.6,
-            child:Padding(padding:EdgeInsets.only(top: mediaQueryData.size.height*.55, left: mediaQueryData.size.width*.4),child:ElevatedButton(onPressed: (){}, child: Text("Add")))
-            ),
+                width: mediaQueryData.size.width > 400
+                    ? mediaQueryData.size.width * 0.6
+                    : mediaQueryData.size.width * 0.7,
+                height: mediaQueryData.size.height * .6,
+                child: Padding(
+                    padding: EdgeInsets.only(
+                        top: mediaQueryData.size.height * .55,
+                        left: mediaQueryData.size.width * .4),
+                    child:
+                        ElevatedButton(onPressed: () {}, child: Text("Add")))),
+            Padding(
+                padding: EdgeInsets.only(top: 30),
+                child: Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context)=>NavBar()));},
+                      icon: Icon(Icons.arrow_back),
+                      iconSize: 40,
+                    ),
+                    const Text("Add Item", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),)
+                  ],
+                ))
           ],
         ),
       ),
     );
   }
-
-
 }
