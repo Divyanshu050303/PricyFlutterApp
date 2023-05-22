20e45118413ac3812d3ea5bda9565e7391a5a9eb
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2023-05-22 21:29:20 +0530

    remove the unused code

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pricyapp/AddPage.dart';
-import 'package:pricyapp/Sidebar.dart';
-import 'package:pricyapp/Calculator.dart';
-import 'package:pricyapp/homePage.dart';
-
+import 'package:pricyapp/NavBar.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -17,63 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      // home: const MyHomePage(title: 'Home Page'),
-      //  home: Calculator(),
-        home: Home(),
-//     home:  Home(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: Colors.blue[400],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Hello World',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-
-      bottomNavigationBar: BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        child: Container(height: 50.0),
-        color: Colors.deepOrangeAccent[200],
-      ),
-      backgroundColor: Colors.red[100] ,
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+        home: NavBar(),
     );
   }
 }
