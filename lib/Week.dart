c73b3ad2e3685940349d28f10cc08a9fcdd685e4
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-09-02 08:59:22 +0530

    Change the whole ui

--- lib/Week.dart
+++ lib/Week.dart
@@ -23,230 +23,194 @@ class _WeakPageState extends State<WeakPage> {
     _child = const WeakPage();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     MediaQueryData mediaQueryData = MediaQuery.of(context);
-    return  Scaffold(
-
-      body: Stack(
-        children: [
-          Container(
-              padding: const EdgeInsets.only(top: 20),
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [Colors.cyan, Colors.purpleAccent],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
+    return Scaffold(
+      body: Container(
+        width: mediaQueryData.size.width,
+        height: mediaQueryData.size.height,
+        color: Colors.black,
+        child: Stack(
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(25),
+                  bottomRight: Radius.circular(25)),
+              child: Container(
+                width: mediaQueryData.size.width,
+                height: mediaQueryData.size.height * 0.12,
+                color: Colors.cyan.shade100,
+                child:   Padding(
+                  padding: EdgeInsets.only(top: 18.0),
+                  child:  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: mediaQueryData.size.width*0.2),
+                        child: const Text(
+                          "Expense Tracker",
+                          style: TextStyle(
+                              color: Colors.black,
+                              decoration: TextDecoration.none,
+                              fontSize: 25),
+                        ),
+                      ),
+                      SizedBox(width: mediaQueryData.size.width*0.1,),
+                      const Icon(Icons.add_chart_rounded, size: 35,)
+                    ],
+                  ),
                 ),
               ),
-              width: mediaQueryData.size.width,
-              height: mediaQueryData.size.height,
-              child: const Text(
-                "Week Overview",
-                style: TextStyle(
-                  fontSize: 35,
-                  color: Colors.black,
-                ),
-                textAlign: TextAlign.center,
-              )),
-          const SizedBox(
-            height: 1,
-          ),
-          Transform.translate(
-              offset: Offset(0, -100),
-              child: Container(
-                padding:
-                EdgeInsets.only(top: mediaQueryData.size.height * 0.001),
-                // alignment: Alignment.topCenter,
-                // transform: Matrix4.rotationY(-0.3),
-
-                transformAlignment: Alignment.topCenter,
-                margin: const EdgeInsets.only(top: 2),
-
-                child:  PieChart(
-                  dataMap: const {
-                    "Flutter": 5,
-                    "React": 3,
-                    "Xamarin": 3,
-                    "Ionic": 2,
-                  },
-                  colorList: [Colors.purpleAccent.shade100,Colors.greenAccent.shade200,Colors.pinkAccent.shade200,Colors.blue.shade500],
-                  animationDuration: Duration(milliseconds: 800),
-                  chartType: ChartType.ring,
-                  ringStrokeWidth: 37,
-                  centerText: "Expense",
-                  centerTextStyle: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                      backgroundColor: Colors.transparent),
-                  chartRadius: 250,
-                  chartValuesOptions: const ChartValuesOptions(
-                    showChartValueBackground: false,
-                    showChartValues: true,
-                    showChartValuesInPercentage: true,
-                    showChartValuesOutside: true,
-                    decimalPlaces: 1,
-                  ),
-                  legendOptions: const LegendOptions(
-                    showLegends: false,
-                    showLegendsInRow: false,
-                    legendPosition: LegendPosition.right,
-                    legendTextStyle: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              )),
-          Container(
-            padding: EdgeInsets.only(
-                top: mediaQueryData.size.height * 0.46,
-                left: mediaQueryData.size.width * .01),
-            // alignment: Alignment.bottomCenter,
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 70,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    SizedBox(
-                        width: mediaQueryData.size.width*0.4,
-                        height: mediaQueryData.size.height*.06,
-                        child:ElevatedButton(
-                          onPressed: () {},
-                          style: ButtonStyle(
-                            backgroundColor:
-                            MaterialStateProperty.resolveWith<Color>(
-                                  (Set<MaterialState> states) {
-                                if (states.contains(MaterialState.pressed)) {
-                                  return Colors.blue;
-                                } else if (states
-                                    .contains(MaterialState.disabled)) {
-                                  return Colors.grey;
-                                }
-                                return Colors.purpleAccent.shade100;
-                              },
-                            ),
-
-                          ),
-
-                          child: const Text("Grocery",
-                              style: TextStyle(fontSize: 20)),
-                        )),    SizedBox(
-                        width: mediaQueryData.size.width*0.4,
-                        height: mediaQueryData.size.height*.06,
-                        child:ElevatedButton(
-                          onPressed: () {},
-                          style: ButtonStyle(
-                            backgroundColor:
-                            MaterialStateProperty.resolveWith<Color>(
-                                  (Set<MaterialState> states) {
-                                if (states.contains(MaterialState.pressed)) {
-                                  return Colors.blue;
-                                } else if (states
-                                    .contains(MaterialState.disabled)) {
-                                  return Colors.grey;
-                                }
-                                return Colors.greenAccent.shade200;
-                              },
-                            ),
-
-                          ),
-
-                          child: const Text("Petroleum",
-                              style: TextStyle(fontSize: 20)),
-                        )),
-
-                  ],
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    SizedBox(
-                        width: mediaQueryData.size.width*0.4,
-                        height: mediaQueryData.size.height*.06,
-                        child:ElevatedButton(
-                          onPressed: () {},
-                          style: ButtonStyle(
-                            backgroundColor:
-                            MaterialStateProperty.resolveWith<Color>(
-                                  (Set<MaterialState> states) {
-                                if (states.contains(MaterialState.pressed)) {
-                                  return Colors.blue;
-                                } else if (states
-                                    .contains(MaterialState.disabled)) {
-                                  return Colors.grey;
-                                }
-                                return Colors.pinkAccent.shade200;
-                              },
-                            ),
-
-                          ),
-
-                          child: const Text("Dairy",
-                              style: TextStyle(fontSize: 20)),
-                        )),
-                    SizedBox(
-                        width: mediaQueryData.size.width*0.4,
-                        height: mediaQueryData.size.height*.06,
-                        child:ElevatedButton(
-                          onPressed: () {},
-                          style: ButtonStyle(
-                            backgroundColor:
-                            MaterialStateProperty.resolveWith<Color>(
-                                  (Set<MaterialState> states) {
-                                if (states.contains(MaterialState.pressed)) {
-                                  return Colors.blue;
-                                } else if (states
-                                    .contains(MaterialState.disabled)) {
-                                  return Colors.grey;
-                                }
-                                return Colors.blue.shade500;
-                              },
-                            ),
-
-                          ),
-
-                          child: const Text("Other",
-                              style: TextStyle(fontSize: 20)),
-                        )),
-                  ],
-                ),    Padding(
-                  padding: EdgeInsets.only(
-                      top: mediaQueryData.size.height * 0.04,
-                      left: mediaQueryData.size.width * 0.7),
-                  child: Container(
-                    width: mediaQueryData.size.width*0.15,
-                    height: mediaQueryData.size.width*0.15,
-                    decoration: BoxDecoration(
-                      color: Colors.cyan,
-                      shape: BoxShape.circle,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.5),
-                          offset: Offset(0, 2),
-                          blurRadius: 6,
-                          spreadRadius: 2,
-                        ),
-                      ],
-                    ),
-                    child: IconButton(
-                      onPressed: () {Navigator.push(context,MaterialPageRoute(builder: (context)=>const AddItem()));},
-                      icon: const Icon(Icons.add),
-                      iconSize: 30,
-                    ),
-                  ),
-                )
-              ],
             ),
-          )
-        ],
-      ),
+            Padding(
+              padding:   EdgeInsets.only(top: mediaQueryData.size.height*0.15, left: mediaQueryData.size.width*0.27),
+              child: Text(
+                "Weekly Expense",
+                style: TextStyle(
+                    fontSize: 23,
+                    decoration: TextDecoration.none,
+                    color: Colors.cyan.shade50),
+              ),
+            ),
+            Transform.translate(
+                offset: const Offset(3, -110,),
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Container(
 
+
+                    padding:
+                    EdgeInsets.only(top: mediaQueryData.size.height * 0.18, bottom: 0),
+                    child: PieChart(
+                      dataMap: const {
+                        "Flutter": 5,
+                        "React": 3,
+                        "Xamarin": 3,
+                        "Ionic": 2,
+                      },
+                      colorList: [
+                        Colors.tealAccent.shade200,
+                        Colors.pink.shade400,
+                        Colors.cyanAccent.shade700,
+                        Colors.blue.shade500
+                      ],
+                      animationDuration: const Duration(milliseconds: 800),
+                      chartType: ChartType.ring,
+                      ringStrokeWidth: 37,
+                      centerText: "Expense",
+                      centerTextStyle: const TextStyle(
+                        color: Colors.cyan,
+                        fontSize: 25,
+                      ),
+                      chartRadius: 250,
+                      chartValuesOptions: const ChartValuesOptions(
+                        showChartValueBackground: true,
+                        showChartValues: true,
+                        showChartValuesInPercentage: true,
+                        showChartValuesOutside: true,
+                        decimalPlaces: 1,
+                      ),
+                      legendOptions: const LegendOptions(
+                        showLegends: false,
+                        showLegendsInRow: false,
+                        legendPosition: LegendPosition.top,
+                        legendTextStyle: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                )),
+
+            Expanded(
+              child: Padding(
+                padding:   EdgeInsets.only(top:mediaQueryData.size.height*0.71),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      width: mediaQueryData.size.width * 0.4,
+                      height: mediaQueryData.size.height * 0.07,
+                      decoration: BoxDecoration(
+                          color: Colors.tealAccent.shade200,
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Colors.white, // Shadow color
+                              spreadRadius: 2, // Spread radius
+                              blurRadius: 2, // Blur radius
+                              offset: Offset(0,1.5),// Offset in the x and y direction
+                            ),
+                          ]
+                      ),
+                      child: const Center(child: Text("Dairy",style: TextStyle(decoration: TextDecoration.none, fontSize: 20),)),
+                    ),
+                    Container(
+                      width: mediaQueryData.size.width * 0.4,
+                      height: mediaQueryData.size.height * 0.07,
+                      decoration: BoxDecoration(
+                          color: Colors.pink.shade400,
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Colors.white, // Shadow color
+                              spreadRadius: 2, // Spread radius
+                              blurRadius: 2, // Blur radius
+                              offset: Offset(0,1.5), // Offset in the x and y direction
+                            ),
+                          ]
+                      ),
+                      child: const Center(child: Text("Petroleum",style: TextStyle(decoration: TextDecoration.none, fontSize: 20),)),
+                    )
+                  ],
+                ),
+              ),
+            ),
+
+            Expanded(child: Padding(
+              padding:   EdgeInsets.only(top: mediaQueryData.size.height*0.8 ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    width: mediaQueryData.size.width * 0.4,
+                    height: mediaQueryData.size.height * 0.07,
+                    decoration: BoxDecoration(
+                        color: Colors.cyanAccent.shade700,
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.white, // Shadow color
+                            spreadRadius: 2, // Spread radius
+                            blurRadius: 2, // Blur radius
+                            offset: Offset(0,1.5),// Offset in the x and y direction
+                          ),
+                        ]
+                    ),
+                    child: const Center(child: Text("Grocery",style: TextStyle(decoration: TextDecoration.none, fontSize: 20),)),
+                  ),
+
+                  Container(
+                    width: mediaQueryData.size.width * 0.4,
+                    height: mediaQueryData.size.height * 0.07,
+
+                    decoration: BoxDecoration(
+                        color: Colors.blue.shade500,
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.white, // Shadow color
+                            spreadRadius: 2, // Spread radius
+                            blurRadius: 2, // Blur radius
+                            offset: Offset(0,1.5), // Offset in the x and y direction
+                          ),
+                        ]
+                    ),
+                    child: const Center(child: Text("Other",style: TextStyle(decoration: TextDecoration.none, fontSize: 20),)),
+                  )
+                ],
+              ),
+            ))
+          ],
+        ),
+      ),
     );
   }
 
